f8e0ab3316b99fcdaf35181777096f74e68cf156
Shakarax <shakarax@gmail.com>
2016-07-24 12:20:07 -0700

    Finished Raycasting

---
+++ Assets/Scripts/RayCast.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RayCast : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+        float rayDistance;
+
+        //Debug make the ray visible
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 2;
+        Debug.DrawRay(transform.position,forward,Color.green);
+
+        if (Physics.Raycast(transform.position, (forward), out hit))
+        {
+            rayDistance = hit.distance;
+            print(rayDistance + " " + hit.collider.gameObject.name);
+        }
+    }
+}
