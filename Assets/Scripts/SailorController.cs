c1a935cae0663a13aa7e21bf28d95e2aeaad2315
Catherine Morgan <morgcat11@gmail.com>
2016-07-23 21:50:31 -0700

    sailors drown

--- Assets/Scripts/SailorController.cs
+++ Assets/Scripts/SailorController.cs
@@ -28,12 +28,15 @@ public class SailorController : MonoBehaviour {
         }
 	}
 
-    void OnCollisionEnter(Collision col)
+    void OnCollisionStay(Collision col)
     {
         if (col.transform.tag == "Water")
         {
-            drownCountdown++;
-            Debug.Log("stay in water for two turns");
+            if (_levelController.CurrentState == LevelController.TurnState.Victim)
+            {
+                drownCountdown++;
+                StillAlive();
+            }
         }
     }
 
