764c9e788a12a2064aba194da6e1f7cd12a5f2e6
Catherine Morgan <morgcat11@gmail.com>
2016-07-24 18:15:49 -0700

    start scene

---
+++ Assets/Scripts/StartController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(Screen.width -100, Screen.height -100, 100, 50), "Start Game"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
